fb00a69daea00401d08918678766fdbcedc1c2c6
Mehtab-Ali <rmehtabali@gmail.com>
2024-01-19 20:02:10 -0500

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+
+    return   <nav>
+        <a href="/">Mehtab-Ali Rajput</a>
+    <div>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Project</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+
+}
